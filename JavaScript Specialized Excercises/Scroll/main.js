458c4cdaf4b3707867e5b9ef835b80968dd605f6
JeromeJoh <jerome.yzhao@foxmail.com>
2022-07-19 19:55:40 +0800

    22-07-18 finished

---
+++ JavaScript Specialized Excercises/Scroll/main.js
@@ -0,0 +1,33 @@
+const divs = document.getElementsByTagName('div');
+const spans = document.getElementsByTagName('span');
+
+//Double 展示内容
+spans[0].innerHTML = spans[0].innerHTML + spans[0].innerHTML
+spans[1].innerHTML = spans[1].innerHTML + spans[1].innerHTML
+
+//滾動函數
+const roll = () => {
+    console.log(divs[0].offsetWidth/2);
+    for(var i=0;i<divs.length;i++) {
+        if(divs[i].scrollLeft > spans[i].offsetWidth/2) {
+            divs[i].scrollLeft = 0;
+        } else {
+            divs[i].scrollLeft = divs[i].scrollLeft + 1;
+        }
+    }
+}
+
+
+
+const timer =setInterval(roll, 10);
+
+
+
+
+
+
+
+
+
+
+
