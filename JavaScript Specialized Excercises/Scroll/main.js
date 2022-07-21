ccd19be01af4be2dd8033314b148468f687e6bae
JeromeJoh <jerome.yzhao@foxmail.com>
2022-07-21 19:51:53 +0800

    22-07-21 finished

--- JavaScript Specialized Excercises/Scroll/main.js
+++ JavaScript Specialized Excercises/Scroll/main.js
@@ -7,7 +7,6 @@ spans[1].innerHTML = spans[1].innerHTML + spans[1].innerHTML
 
 //滾動函數
 const roll = () => {
-    console.log(divs[0].offsetWidth/2);
     for(var i=0;i<divs.length;i++) {
         if(divs[i].scrollLeft > spans[i].offsetWidth/2) {
             divs[i].scrollLeft = 0;
